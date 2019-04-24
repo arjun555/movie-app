6d63c187be2824b9074289e46b030c0b82336de5
Arjun <arjunpatel555@gmail.com>
2019-04-24 15:27:21 +1000

    changed movie data references to single variable

--- main.rb
+++ main.rb
@@ -11,19 +11,17 @@ end
 get '/movie' do
   movie = params[:title];
   # @result = HTTParty.get('http://omdbapi.com/?apikey=2f6435d9&t=jaws')
-  movie_data = HTTParty.get('http://omdbapi.com/?apikey=2f6435d9&t='+ movie)
-  # @result = HTTParty.get(`http://omdbapi.com/?apikey=2f6435d9&t=#{movie}`)
-  # binding.pry
-  @result = movie_data
-  @title = movie_data["Title"]
-  @year = movie_data["Year"]
-  @rated = movie_data["Rated"]
-  @runtime = movie_data["Runtime"]
-  @genre = movie_data["Genre"]
-  @poster = movie_data["Poster"]
-  @plot = movie_data["Plot"]
-  @director = movie_data["Director"]
-  @actors = movie_data["Actors"]
+  @movie_data = HTTParty.get('http://omdbapi.com/?apikey=2f6435d9&t='+ movie)
+  # @result = movie_data
+  # @title = movie_data["Title"]
+  # @year = movie_data["Year"]
+  # @rated = movie_data["Rated"]
+  # @runtime = movie_data["Runtime"]
+  # @genre = movie_data["Genre"]
+  # @poster = movie_data["Poster"]
+  # @plot = movie_data["Plot"]
+  # @director = movie_data["Director"]
+  # @actors = movie_data["Actors"]
 
   erb :movies
 end
