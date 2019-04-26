aa7f0f983da1acef23a94531674dffc559abca5b
Arjun <arjunpatel555@gmail.com>
2019-04-26 12:38:06 +1000

    tidy ruby code

--- main.rb
+++ main.rb
@@ -9,19 +9,42 @@ end
 
 
 get '/movie' do
-  movie = params[:title];
-  puts "movie = #{movie}"
-  # @movie_data = HTTParty.get('http://omdbapi.com/?apikey=2f6435d9&t=Jaws')
-  @movie_data = HTTParty.get('http://omdbapi.com/?apikey=2f6435d9&t='+ movie)
-  # @movie_data = HTTParty.get('http://omdbapi.com/?apikey=2f6435d9&plot=full&s='+ movie)
+  @title_query = params[:title];
+  @movie_data = HTTParty.get('http://omdbapi.com/?apikey=2f6435d9&plot=full&t='+ @title_query)
   erb :movie
 end
 
 get '/search' do
-  title_query = params[:query]
-  @search_results = HTTParty.get('http://omdbapi.com/?apikey=2f6435d9&s='+ title_query)
-  erb :search_results
+  @title_query = params[:query]
+  @search_results = HTTParty.get('http://omdbapi.com/?apikey=2f6435d9&s='+ @title_query)
+
+  if is_search_successful() && number_of_movies() <= 1
+    @movie_data = HTTParty.get('http://omdbapi.com/?apikey=2f6435d9&plot=full&t='+ @title_query)
+    erb :movie
+  else
+    erb :search_results
+  end
 end
 
+
+def number_of_movies
+  movie_count = 0;
+  @search_results["Search"].each do |result| 
+    if result["Type"] == "movie"
+      movie_count += 1
+    end
+  end
+  return movie_count
+end
+
+def is_search_successful
+  if @search_results["Response"] == "True"
+    @search_successful = true
+    return true
+  else
+    @search_successful = false
+    return false
+  end
+end
 
 
